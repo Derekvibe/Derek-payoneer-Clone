f601423dba46e3923c46d84cd349d6f6b4cb481b
Derek Emmanuel <derekemmanuel99@gmail.com>
2023-03-31 00:48:45 +0100

    latest commit

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// JS CODE FOR NAVIGATION BAR
+const action_btn = document.querySelector ('.action_btn')
+const action_btnicon = document.querySelector ('.action_btn i')
+const dropdown = document.querySelector ('.drop_down')
+
+action_btn.onclick = function (){
+    dropdown.classList.toggle('open')
+
+    const opened = dropdown.classList.contains('open')
+
+    action_btnicon.classList = opened
+    ?'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
+}
+
+// ENDS HERE
+
 let names = document.getElementById('name')
 let mail = document.getElementById('mail')
 let phone = document.getElementById('phone')
